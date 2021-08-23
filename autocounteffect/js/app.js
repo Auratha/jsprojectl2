a2399396abafa9fb5a1d8dbb1abd0cfe85dd02e6
Au Ra Tha <auratha1000@gmail.com>
2021-08-23 11:49:08 +0630

    first commit

---
+++ autocounteffect/js/app.js
@@ -0,0 +1,30 @@
+//UI
+const counters = document.querySelectorAll(".counter");
+// console.log(counters);
+counters.forEach(counter=>{
+    // console.log(counter);
+    counter.innerText = "0";
+    // console.log(counter.getAttribute("data-target"));
+
+    const updatecounter = () => {
+        //const target = + counter.getAttribute('data-target');
+
+        const target = Number(counter.getAttribute('data-target'));
+        // console.log(typeof target);
+
+        const ctr = +counter.innerText;
+        // console.log(typeof ctr);
+
+        const increment = target/100;
+        // console.log(increment);
+
+        if(ctr < target){
+            counter.innerText = Math.ceil(ctr+increment);
+            console.log(ctr);
+            setTimeout(updatecounter,30);
+        }
+    }
+
+    updatecounter();
+});
+
